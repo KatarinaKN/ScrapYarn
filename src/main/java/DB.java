fd12123fa01a8a20a9423a96696cbe6dad80823e
stinetorndal <cph-sc95@stud.ek.dk>
2025-12-09 11:06:28 +0100

    kolonner tilføjet

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,2 +1,11 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DB {
+    static Connection connect() throws SQLException {
+        Connection connection = DriverManager.getConnection("C:/Users/stine/Documents/DAT1/Java/ScrapYarn/src/main/resources");
+        return connection;
+    }
+
 }
