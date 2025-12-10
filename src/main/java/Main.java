772d324674c23327a1fb1476dc5ba9c2315240c9
stinetorndal <cph-sc95@stud.ek.dk>
2025-12-10 17:09:35 +0100

    Test af metode og database_2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,15 @@
+import java.util.List;
+
 public class Main {
     static void main() {
-
         DBInquiries dbInquiries = new DBInquiries("jdbc:sqlite:src/main/resources/patterns.db");
 
         dbInquiries.getCrochetPatternByLevel("beginner");
 
+        }
+
+
 
 
     }
-}
+
