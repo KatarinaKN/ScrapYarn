966659408e8de7964390949a8c1204840d873827
KatarinaKN <katarinak.nielsen@gmail.com>
2025-12-10 12:49:40 +0100

    StartMenu class added

---
+++ src/main/java/StartMenu.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+public class StartMenu {
+
+    //TODO vi skal et sted i vores program have en scanner, som kan læse brugerens svar.
+
+    //TODO er det rigtigt, at alle vores metoder, der stiller spørgsmål, skal returnere noget?
+
+    public void startSession(){
+
+    }
+
+    public int knitOrCrochet(){
+        return 0;
+    }
+
+    public int chooseLevel(){
+        return 0;
+    }
+
+    public int amountOfYarn(){
+        return 0;
+    }
+
+    public int chooseNeedleSize(){
+        return 0;
+    }
+
+    public int chooseYarnType(){
+        return 0;
+    }
+
+    public int chooseGauge(){
+        return 0;
+    }
+
+    public ArrayList<Pattern> showAllPatterns(){
+        return allPatterns;
+    }
+
+    //Spørger brugeren, om man vil prøve igen (hvis man ikke er tilfreds med de foreslåede opskrifter.
+    public boolean tryAgain(){
+        return true;
+    }
+
+    //Giver brugeren en tilfældig opskrift.
+    public Pattern getRandomPattern(){
+        return ;
+    }
+
+    public void endSession(){
+
+    }
+}
