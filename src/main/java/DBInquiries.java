fe2b9070588e3fc440e8b312bb17c80cf2316757
stinetorndal <cph-sc95@stud.ek.dk>
2025-12-11 17:44:26 +0100

    pdf åbner ENDELIG

--- src/main/java/DBInquiries.java
+++ src/main/java/DBInquiries.java
@@ -4,15 +4,16 @@ import java.util.List;
 
 public class DBInquiries {
 
+
     public List<String> getCrochetPatternByLevel(String level) {
         List<String> patterns = new ArrayList<>();
         String sql = "SELECT Name FROM pattern WHERE Level = ?";
 
         try (Connection connection = DBConnection.getConnection();
-             PreparedStatement stmt = connection.prepareStatement(sql)) {
+             PreparedStatement statement = connection.prepareStatement(sql)) {
 
-            stmt.setString(1, level);
-            try (ResultSet resultSet = stmt.executeQuery()) {
+            statement.setString(1, level);
+            try (ResultSet resultSet = statement.executeQuery()) {
                 while (resultSet.next()) {
                     patterns.add(resultSet.getString("Name"));
                 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,18 @@
-import java.util.List;
+    import java.io.File;
+    import java.util.List;
 
-public class Main {
-    public static void main(String[] args) {
-        DBInquiries db = new DBInquiries();
-        List<String> beginners = db.getCrochetPatternByLevel("Beginner");
 
-        System.out.println("Begynder-opskrifter:");
-        for (String name : beginners) {
-            System.out.println(name);
+    public class Main {
+        public static void main(String[] args)  {
+
+            OpenPdfTest.openPdfById(1);
+            DBInquiries db = new DBInquiries();
+            List<String> beginners = db.getCrochetPatternByLevel("Beginner");
+
+            System.out.println("Begynder-opskrifter:");
+            for (String name : beginners) {
+                System.out.println(name);
+            }
+
         }
     }
-}
