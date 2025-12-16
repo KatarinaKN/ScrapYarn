0ca935f121e0a963efafd27e75989b63597b5208
stinetorndal <cph-sc95@stud.ek.dk>
2025-12-16 15:41:32 +0100

    FINAL VERSION

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -4,12 +4,12 @@ import java.nio.file.*;
 public class DBConnection {
     public static Connection getConnection() throws SQLException {
         try {
-            //------Classloader er Javas ressource-finder, den indlæser DBConnection
-            //------og kan også finde pdf'er, billeder osv.
-            //------Den er alternativet til hardkodede stier fx C:/user/...
-            //------Path er et java-interface, derfor Paths. Er en factory-metode
+            //------Classloader er Javas ressource-finder, den indlæser DBConnection------
+            //------og kan også finde pdf'er, billeder osv.------
+            //------Den er alternativet til hardkodede stier fx C:/user/...--------
+            //------Path er et java-interface, derfor Paths. Er en factory-metode------
             Path path = Paths.get(DBConnection.class.getClassLoader()
-                    //------URI = Uniform Resource  Identifier. Java skelner mellem URL og URI
+                    //------URI = Uniform Resource  Identifier. Java skelner mellem URL og URI------
                     //------URI er mere generel, hvor URL er netværksorienteret
                     .getResource("patterns.db").toURI());
             //System.out.println("Java åbner databasefilen her: " + path.toAbsolutePath());
